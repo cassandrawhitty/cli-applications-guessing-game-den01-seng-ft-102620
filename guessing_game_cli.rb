42f7d6bbe18541ecab5c3e3086590e64803e3c25
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-22 13:15:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,14 +6,14 @@ def run_guessing_game
   random_number = rand(6) + 1
   puts "Guess a number between 1 and 6!"
   input = get_user_input
+  if input == "exit" 
+    puts "Goodbye!"
+  end
   if input == random_number
     puts "You guessed the correct number!"
   end
   if input != random_number
     puts "Sorry! The computer guessed #{random_number}."
-  end
-  if input == "exit" 
-    puts "Goodbye!"
   end
 end
 
